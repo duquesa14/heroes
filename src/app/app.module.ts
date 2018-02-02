abde5c95ee263f22c3c14e602b8c58f458b0109c
Maureen <duquemaureen5@gmail.com>
2018-02-01 21:29:48 -0430

    Busqueda de heroes
    * Se añadio un componente de busqueda de heroes
    * Se incluyeron imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+//Rutas
+import {APP_ROUTING} from './app.routes';
 
+//Servicios
+import {HeroesService} from './servicios/heroes.services';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent,
+    HomeComponent,
+    HeroeComponent,
+    BuscadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
